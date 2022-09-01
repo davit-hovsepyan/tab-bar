371353200f238038edf59c963b6aef67103a41f5
Davit <hovsepyan.davit93@gmail.com>
2022-09-01 21:10:34 +0400

    new

--- TapBar2/ViewController.swift
+++ TapBar2/ViewController.swift
@@ -6,14 +6,27 @@
 //
 
 import UIKit
+class CustomTabBar: UITabBar {
 
-class ViewController: UIViewController {
+    let roundedView = UIView(frame: .zero)
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    override func awakeFromNib() {
+        super.awakeFromNib()
+
+        roundedView.layer.masksToBounds = true
+        roundedView.layer.cornerRadius = 12.0
+        roundedView.layer.borderWidth = 2.0
+        roundedView.isUserInteractionEnabled = false
+        roundedView.layer.borderColor = UIColor.black.cgColor
+        self.addSubview(roundedView)
     }
 
+    override func layoutSubviews() {
+        super.layoutSubviews()
 
+        let margin: CGFloat = 12.0
+        let position = CGPoint(x: margin, y: 0)
+        let size = CGSize(width: self.frame.width - margin * 2, height: self.frame.height)
+        roundedView.frame = CGRect(origin: position, size: size)
+    }
 }
-
